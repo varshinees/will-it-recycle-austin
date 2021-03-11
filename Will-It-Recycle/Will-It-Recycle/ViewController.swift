fd827ceb1b71d151d15ebe2cbbe73bc4937f9811
Varshinee Sreekanth <varshinee@utexas.edu>
2021-03-11 15:33:51 -0600

    created xcode project and split storyboards

---
+++ Will-It-Recycle/Will-It-Recycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Will-It-Recycle
+//
+//  Created by Varshinee - School on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
