c66050566f4bc88bd4c5009213d9686e69230a6b
Chelsie Barrientos <chelsie_barrientos@Chelsies-MBP.GALAWAY>
2021-03-26 10:36:05 -0500

    Pushing master branch

--- Will-It-Recycle/Will-It-Recycle/WelcomeViewController.swift
+++ Will-It-Recycle/Will-It-Recycle/WelcomeViewController.swift
@@ -6,24 +6,21 @@
 //
 
 import UIKit
+import MaterialComponents
+import MaterialComponents.MaterialButtons
+import MaterialComponents.MaterialButtons_Theming
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var recycleButton: MDCButton!
+    
+    let buttonScheme = MDCButtonScheme()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        MDCContainedButtonThemer.applyScheme(buttonScheme, to: recycleButton)
+        
+        recycleButton.setTitle("Start Recycling!", for: .normal)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
