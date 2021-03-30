9d6a89a1163f98c84d444fb68cbbd40b9216ef2e
Chelsie Barrientos <csant1391@outlook.com>
2021-03-29 19:04:17 -0500

    edited settings

--- Will-It-Recycle/Will-It-Recycle/SettingsViewController.swift
+++ Will-It-Recycle/Will-It-Recycle/SettingsViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Firebase
 import MaterialComponents
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialButtons_Theming
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var changeEmail: UITextField!
+    
     @IBOutlet weak var updateButton: MDCButton!
     
     override func viewDidLoad() {
@@ -24,4 +27,23 @@ class SettingsViewController: UIViewController {
 
     }
 
+    @IBAction func updatePressed(_ sender: Any) {
+        
+        if (changeEmail.text != "") {
+            Auth.auth().currentUser?.updateEmail(to: changeEmail.text!, completion: nil)
+        }
+    }
+    
+//    @IBAction func signOut(_ sender: Any) {
+//        do { try Auth.auth().signOut() }
+//            catch { print("already logged out") }
+//
+            // login, add segue to main.storyboard in settings storyboard
+//        performSegue(withIdentifier: "BackToStart", sender: nil)
+        
+//        let loginVC = LoginViewController()
+//        present(loginVC, animated: false, completion: nil)
+            
+//            navigationController?.popToRootViewController(animated: true)
+    }
 }
