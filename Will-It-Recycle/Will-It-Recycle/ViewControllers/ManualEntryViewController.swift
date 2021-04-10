8e66d74fbf6da1010495efe9fe8f212e7f7645f3
Varshinee Sreekanth <varshinee@utexas.edu>
2021-04-10 10:27:54 -0500

    Added history stuff + changed pods

--- Will-It-Recycle/Will-It-Recycle/ViewControllers/ManualEntryViewController.swift
+++ Will-It-Recycle/Will-It-Recycle/ViewControllers/ManualEntryViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialList
+import Firebase
 
 public struct ManualEntryItem: Hashable {
     let value: Int
@@ -17,6 +18,15 @@ public struct ManualEntryItem: Hashable {
         self.name = name
         self.value = value
     }
+}
+
+public struct UserObject: Hashable {
+    let allTimeLeaves: Int
+    let currentLeaves: Int
+    let displayName: String
+    let leaderboard: Int
+    let notifications: Int
+    let rewardsForToday: Int
 }
 
 public var manualEntryData = [
@@ -40,8 +50,6 @@ class RecycleDropdownCell: UITableViewCell {
 
 class ManualEntryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
-
-//    @IBOutlet weak var backBtn: MDCButton!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var redeemBtn: MDCButton!
     @IBOutlet weak var tableView: UITableView!
@@ -50,9 +58,7 @@ class ManualEntryViewController: UIViewController, UITableViewDataSource, UITabl
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        backBtn.setTitle("< BACK", for: .normal)
-//        backBtn.setTitleColor(UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0), for: .normal)
+
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -98,11 +104,8 @@ class ManualEntryViewController: UIViewController, UITableViewDataSource, UITabl
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        do {
-            try quantities[textField.tag] = Int(textField.text!) ?? 0
-        } catch {
-            return
-        }
+        quantities[textField.tag] = Int(textField.text!) ?? 0
+
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -111,23 +114,89 @@ class ManualEntryViewController: UIViewController, UITableViewDataSource, UITabl
     
     
     @IBAction func redeemButtonPressed(_ sender: Any) {
-        for quantity in quantities {
-            // reset all textFields
-            quantities = [Int](repeating: 0, count: manualEntryData.count)
-            
+        var leaves = 0
+        var ref: DatabaseReference!
+        ref = Database.database().reference()
+        
+        // get the current date and time
+        let currentDateTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        let dateString = formatter.string(from: currentDateTime)
+        
+        for i in 0...quantities.count-1 {
             // calculate number of leaves earned
+            leaves += quantities[i] * manualEntryData[i].value
+            
+            if (quantities[i] > 0) {
+                
+                // update name + quantity + date to firebase
+                guard let key = ref.child("users/\(Auth.auth().currentUser!.uid)/").childByAutoId().key else { return }
+                
+                let item = ["date": dateString,
+                            "name": manualEntryData[i].name,
+                            "quantity": quantities[i]] as [String : Any]
+                let childUpdates = ["users/\(Auth.auth().currentUser!.uid)/history/\(key)": item]
+                ref.updateChildValues(childUpdates)
+            }
+            
+        }
+        
+        // if no leaves earned, pop up error alert
+        if leaves == 0 {
+            let controller = UIAlertController(
+                title: "No items entered",
+                message: "Please enter at least one quantity",
+                preferredStyle: .alert)
+            
+            controller.addAction(UIAlertAction(
+                                    title: "OK",
+                                    style: .default,
+                                    handler: nil))
+            present(controller, animated: true, completion: nil)
+            return
         }
         
         // update number of leaves and total earned leaves
+        ref.child("users/\(Auth.auth().currentUser!.uid)").getData { (error, snapshot) in
+            if let error = error {
+                print("Error getting data \(error)")
+            }
+            else if snapshot.exists() {
+                guard let user = snapshot.value as? [String: Any] else {
+                      return
+                    }
+                
+                let childUpdates:[String: Int] = ["users/\(Auth.auth().currentUser!.uid)/currentLeaves": Int(user["currentLeaves"] as! Int) + leaves,
+                                    "users/\(Auth.auth().currentUser!.uid)/allTimeLeaves": Int(user["allTimeLeaves"] as! Int) + leaves]
+                ref.updateChildValues(childUpdates)
+            }
+            else {
+                print("User does not exist")
+            }
+        }
+        
+        // send alert that indicates amount earned
+        let controller = UIAlertController(
+            title: "Success! 🌱",
+            message: "You have added \(leaves) leaves to your account.",
+            preferredStyle: .alert)
+        
+        controller.addAction(UIAlertAction(
+                                title: "Continue",
+                                style: .default,
+                                handler: nil))
+        present(controller, animated: true, completion: nil)
+        
+        // reset all textFields
+        quantities = [Int](repeating: 0, count: manualEntryData.count)
+        tableView.reloadData()
     }
     
     // code to enable tapping on the background to remove software keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
-    
-
-
-    
+ 
 }
