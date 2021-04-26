23919a912e594adbaba79c804c31d392665f2609
Chelsie Barrientos <csant1391@outlook.com>
2021-04-26 10:38:35 -0400

    Fixed leaderboard bug

--- Will-It-Recycle/Will-It-Recycle/ViewControllers/DashboardViewController.swift
+++ Will-It-Recycle/Will-It-Recycle/ViewControllers/DashboardViewController.swift
@@ -83,7 +83,7 @@ class DashboardViewController: UIViewController {
             var ref: DatabaseReference!
             ref = Database.database().reference()
              
-            let users = ref.child("users").queryOrdered(byChild: "allTimeLeaves").queryLimited(toLast: 5)
+            let users = ref.child("users").queryOrdered(byChild: "allTimeLeaves").queryLimited(toLast: 6)
 
             users.observe(.value, with: { (snapshot) in
               if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
